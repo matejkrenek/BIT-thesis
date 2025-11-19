10558bd257b632ef8a5ea5d180ef5de2a9aabb5f
matejkrenek <mate23.krenek@gmail.com>
2025-11-19 02:44:26 +0100

    fix(refactor): remve unused files for now

--- project/src/utils/io.py
+++
@@ -1,8 +0,0 @@
-import open3d as o3d
-import numpy as np
-from pathlib import Path
-
-
-def load_ply(path: str | Path) -> np.ndarray:
-    pcd = o3d.io.read_point_cloud(str(path))
-    return np.asarray(pcd.points)
